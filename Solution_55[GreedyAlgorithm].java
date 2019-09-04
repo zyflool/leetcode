d2114bc312ca17ea437b1d24a773372d1e1c902d
zyflool <532256565@qq.com>
2019-09-04 20:37:37 +0800

    0904

---
+++ Solution_55[GreedyAlgorithm].java
@@ -0,0 +1,40 @@
+/*
+55. 跳跃游戏
+给定一个非负整数数组，你最初位于数组的第一个位置。
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+判断你是否能够到达最后一个位置。
+
+示例 1:
+输入: [2,3,1,1,4]
+输出: true
+解释: 从位置 0 到 1 跳 1 步, 然后跳 3 步到达最后一个位置。
+
+示例 2:
+输入: [3,2,1,0,4]
+输出: false
+解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+*/
+class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,
+                0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,
+                2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5};
+        Solution solution = new Solution();
+        System.out.println(solution.canJump(nums));
+    }
+        public boolean canJump(int[] nums) {
+
+            if (nums == null) {
+                return false;
+            }
+            int lastPosition = nums.length - 1;
+            for (int i = nums.length - 1; i >= 0; i--) {
+                // 逐步向前递推
+                if (nums[i] + i >= lastPosition) {
+                    lastPosition = i;
+                }
+            }
+            return lastPosition == 0;
+        }
+}
---
+++ Solution_55[dp].java
@@ -0,0 +1,44 @@
+/*
+55. 跳跃游戏
+给定一个非负整数数组，你最初位于数组的第一个位置。
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+判断你是否能够到达最后一个位置。
+
+示例 1:
+输入: [2,3,1,1,4]
+输出: true
+解释: 从位置 0 到 1 跳 1 步, 然后跳 3 步到达最后一个位置。
+
+示例 2:
+输入: [3,2,1,0,4]
+输出: false
+解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+*/
+class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,
+                0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,
+                2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5};
+        Solution solution = new Solution();
+        System.out.println(solution.canJump(nums));
+    }
+    public boolean canJump(int[] nums) {
+
+        if (nums == null) {
+            return false;
+        }
+        boolean[] dp = new boolean[nums.length];
+        dp[0] = true;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                // 如果之前的j节点可达，并且从此节点可以到跳到i
+                if (dp[j] && nums[j] + j >= i) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[nums.length - 1];
+    }
+}
