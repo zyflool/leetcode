0c687d0114956155588f31b908122eaf1ebda993
zyflool <532256565@qq.com>
2019-10-25 19:01:04 +0800

    1025

---
+++ Solution_95[BackTrack].java
@@ -0,0 +1,81 @@
+/*
+95. 不同的二叉搜索树 II
+给定一个整数 n，生成所有由 1 ... n 为节点所组成的二叉搜索树。
+
+示例:
+
+输入: 3
+输出:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+解释:
+以上的输出对应以下 5 种不同结构的二叉搜索树：
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+/*
+递归
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Solution {
+
+    public static void main(String[] args) {
+        int n = 3;
+        Solution solution = new Solution();
+        System.out.println(solution.generateTrees(n));
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode> ans = new ArrayList<TreeNode>();
+        if (n == 0) {
+            return ans;
+        }
+        return getAns(1, n);
+
+    }
+
+    private List<TreeNode> getAns(int start, int end) {
+        List<TreeNode> ans = new ArrayList<TreeNode>();
+        //此时没有数字，将 null 加入结果中
+        if (start > end) {
+            ans.add(null);
+            return ans;
+        }
+        //只有一个数字，当前数字作为一棵树加入结果中
+        if (start == end) {
+            TreeNode tree = new TreeNode(start);
+            ans.add(tree);
+            return ans;
+        }
+        //尝试每个数字作为根节点
+        for (int i = start; i <= end; i++) {
+            //得到所有可能的左子树
+            List<TreeNode> leftTrees = getAns(start, i - 1);
+            //得到所有可能的右子树
+            List<TreeNode> rightTrees = getAns(i + 1, end);
+            //左子树右子树两两组合
+            for (TreeNode leftTree : leftTrees) {
+                for (TreeNode rightTree : rightTrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = leftTree;
+                    root.right = rightTree;
+                    //加入到最终结果中
+                    ans.add(root);
+                }
+            }
+        }
+        return ans;
+    }
+}
